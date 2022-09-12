3e42c422c52bdca749274f5c629c7198ffaa42c1
amanuel <amanuelzewdu21@gmail.com>
2022-09-12 12:37:25 +0300

    modify model

--- scripts/Applications/model.py
+++ scripts/Applications/model.py
@@ -1,95 +1,32 @@
-from operator import mod
-import sys
-import os
+from numpy.core.records import array
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import warnings
+from joblib import load
+import numpy as np
+import sys
 import pickle
-from sklearn.preprocessing import StandardScaler
 
+def app():
 
-warnings.filterwarnings('ignore')
+    # Load Saved Results Data
+    model = pickle.load(filename='models/10-09-2022-01-13-01-52.92%.pkl')
 
-cwd = os.getcwd()
-sys.path.append(f'{cwd}/scripts/')
+    st.title("Satisfaction Model")
 
-from data_preProcessing import dataProcessor
-from data_cleaning import dataCleaner
-from ltsm_helper import ltsm_time
+    st.header("To calculate a user's satisfaction score, enter values below.")
 
-cwd = os.getcwd()
-sys.path.append(f'{cwd}/scripts/')
+    Customers = st.number_input('Enter sessions', key='a')
+    SalesPerCustomer = st.number_input('Enter total time', key='b')
+    #total_data = st.number_input('Enter total data', key='c')
+    #total_retransmission = st.number_input('Enter tcp retransmission', key='d')
+    #average_delay = st.number_input('Enter average delay', key='e')
+    #total_throughput = st.number_input('Enter average throughput', key='f')
 
-preprocess = dataProcessor()
-cleaner= dataCleaner()
+    if st.button('Satisfaction prediction'):
+        array = [Customers, SalesPerCustomer]
+        """, total_data,
+                 total_retransmission, average_delay, total_throughput]"""
+        val = model.predict([array])
+        satisfaction = [i[0] for i in val][0]
+        st.write(
+            "The user's estimated satisfaction score is: {:.3f}".format(satisfaction))
 
-cols = ['Store', 'Customers', 'Promo', 'StateHoliday', 'SchoolHoliday', 'StoreType', 'Assortment','CompetitionDistance', 'CompetitionOpenSinceMonth', 'CompetitionOpenSinceYear', 'Promo2', 'Promos2SinceWeek', 'Promo2SinceYear','PromoInterval', 'Open','Date', 'DayOfWeek'  ]
-    
-
-
-def plot_predictions(date, sales):
-    fig = plt.figure(figsize=(20, 7))
-    ax = sns.lineplot(x=date, y=sales)
-    ax.set_title("Predicted Sales", fontsize=24)
-    ax.set_xlabel("Row index", fontsize=18)
-    ax.set_ylabel("Sales", fontsize=18)
-    
-    # fig = plt.figure(figsize=(18, 5))
-    
-
-    return fig 
-
-
-
-
-st.title("Rossmann Pharmaceuticals Sales Forecaster")
-
-# @st.cache
-
-
-input_data = pd.DataFrame(columns=cols)
-submitted = False
-
-df = None
-file=True
-data_added = False
-uploaded_file = None
-model = None
-
-
-file=False
-values = []
-
-with st.form(key='inference', clear_on_submit=True):
-        
-    for feature in cols:
-            
-        values.append(st.text_input(feature,key=feature))
-
-        submitted = st.form_submit_button("Show Prediction")
-    
-
-@st.cache
-def load_model():
-    model = pd.read_pickle(r'../../models/10-09-2022-01-13-01-52.92%.pkl')
-    return model
-
-
-if data_added:
-
-    # model = load_model()
-    model = pickle.load(open(f'{cwd}/models/10-09-2022-01-13-01-52.92%.pkl', 'rb'))
-        
-    prediction = model.predict(df)
-    df["Date"]=pd.DatetimeIndex(df["Day"].astype(str)+'-' + df["Month"].astype(str)+'-' + df["Year"].astype(str))
-        
-
-   
-
-    fig = plot_predictions(df['Date'], prediction)
-    download=pd.DataFrame(index=df['Date'],columns=["Sales Prediction"],data=prediction).to_csv()
-    st.pyplot(fig)
-
-    st.download_button("Download CSV",data=download,file_name="Prediction.csv")

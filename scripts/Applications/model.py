64886dc8b92aebdd91a839489b5659220af65aa1
amanuel <amanuelzewdu21@gmail.com>
2022-09-12 13:47:15 +0300

    modify model

--- scripts/Applications/model.py
+++ scripts/Applications/model.py
@@ -9,19 +9,19 @@ def app():
     # Load Saved Results Data
     model = pickle.load(open('models/10-09-2022-01-13-01-52.92%.pkl', 'rb'))
 
-    st.title("Satisfaction Model")
+    st.title("Predicion")
 
-    st.header("To calculate a user's satisfaction score, enter values below.")
+    st.header("To calculate a sales prediction, enter values below.")
 
-    Customers = st.number_input('Enter sessions', key='a')
-    SalesPerCustomer = st.number_input('Enter total time', key='b')
+    Customers = st.number_input('Enter customers', key='a')
+    Promo = st.number_input('Promoted?', key='b')
     #total_data = st.number_input('Enter total data', key='c')
     #total_retransmission = st.number_input('Enter tcp retransmission', key='d')
     #average_delay = st.number_input('Enter average delay', key='e')
     #total_throughput = st.number_input('Enter average throughput', key='f')
 
-    if st.button('Satisfaction prediction'):
-        array = [Customers, SalesPerCustomer]
+    if st.button('Sales prediction'):
+        array = [Customers, Promo]
         """, total_data,
                  total_retransmission, average_delay, total_throughput]"""
         val = model.predict([array])
